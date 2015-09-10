3469dc9c413626e50a001ee4c2213c60e8713153
Kevin Loney <kevin.loney@brainsinjars.com>
2015-09-10 12:28:06 -0600

    Fixed an indentation issue

--- pythonosc/parsing/ntp.py
+++ pythonosc/parsing/ntp.py
@@ -17,22 +17,22 @@ _NTP_DELTA = (_SYSTEM_EPOCH - _NTP_EPOCH).days * 24 * 3600
 class NtpError(Exception):
 	"""Base class for ntp module errors."""
 
-	def ntp_to_system_time(date):
-		"""Convert a NTP time to system time.
+def ntp_to_system_time(date):
+	"""Convert a NTP time to system time.
 
-		System time is reprensented by seconds since the epoch in UTC.
-		"""
-		return date - _NTP_DELTA
+	System time is reprensented by seconds since the epoch in UTC.
+	"""
+	return date - _NTP_DELTA
 
 
-	def system_time_to_ntp(date):
-		"""Convert a system time to a NTP time datagram.
+def system_time_to_ntp(date):
+	"""Convert a system time to a NTP time datagram.
 
-		System time is reprensented by seconds since the epoch in UTC.
-		"""
-		try:
-			ntp = date + _NTP_DELTA
-		except TypeError as ve:
-			raise NtpError('Invalud date: {}'.format(ve))
-		num_secs, fraction = str(ntp).split('.')
-		return struct.pack('>I', int(num_secs)) + struct.pack('>I', int(fraction))
+	System time is reprensented by seconds since the epoch in UTC.
+	"""
+	try:
+		ntp = date + _NTP_DELTA
+	except TypeError as ve:
+		raise NtpError('Invalud date: {}'.format(ve))
+	num_secs, fraction = str(ntp).split('.')
+	return struct.pack('>I', int(num_secs)) + struct.pack('>I', int(fraction))
